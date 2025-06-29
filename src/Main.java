7f1460d3cbe0d8e810f51a431007f5432118baa9
Jonatan Rodríguez <jonodomongos@hotmail.com>
2025-06-29 22:12:31 +0200

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello wordl!");
+
+        System.out.println("Segundo envío desde consola");
     }
 }
